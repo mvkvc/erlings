b9a2ca1586e31325977d3d5ce0807dfb00676acd
Marko Vukovic <mvkvc.git@gmail.com>
2024-06-06 12:34:26 -0400

    wip

--- sequential/hello_pattern/src/hello_pattern.erl
+++ sequential/hello_pattern/src/hello_pattern.erl
@@ -2,5 +2,19 @@
 
 -export([hello/1]).
 
-hello({tuple_element_1, tuple_element_2}) ->
-  resolve.
+% - `{morning, Name}`, ignores the name and returns `morning`.
+% - `{evening, Name}`, returns a tuple `{good, evening, Name}`.
+% - `{night, Name}`, ignores the name and return `night`.
+% - `{math_class, Number, Name}`. If the number is lower than zero, return
+%   `none`, in any other case return `{math_class, Name}`.
+
+hello({morning, Name}) ->
+  morning;
+hello({evening, Name}) ->
+  {good, evening, Name};
+hello({night, Name}) ->
+  night;
+hello({math_class, Number, Name}) when Number < 0 ->
+  none;
+hello({math_class, Number, Name}) ->
+  {math_class, Name}.
